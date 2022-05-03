1af52f6d925102f4a77491a9a2ed9a8b27a4d12e
Marius <marius.budreika09@gmail.com>
2022-05-03 15:09:45 +0300

    card, main components that fetch data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
+import Main from "./components/Main";
 
 function App() {
-  return <div className="App"></div>;
+  const [covidInfo, setCovidInfo] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    fetchCovidStatisticsHandler();
+  }, []);
+
+  console.log(covidInfo);
+
+  async function fetchCovidStatisticsHandler() {
+    setIsLoading(true);
+    const response = await fetch(
+      "https://api.covid19api.com/dayone/country/lithuania"
+    );
+    const data = await response.json();
+
+    const lastObject = data.length - 1;
+    const newestData = data[lastObject];
+    setCovidInfo(newestData);
+    setIsLoading(false);
+  }
+
+  return (
+    <div className="App">
+      {!isLoading && <Main statistics={covidInfo} />}
+      {isLoading && <p>Loading...</p>}
+    </div>
+  );
 }
 
 export default App;
